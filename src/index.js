5ad544f057e199fe33d847d1aefb492c9017d2ae
BykovaAlyona <alena.b.1741@gmail.com>
2022-08-27 16:52:46 +0500

    remove react-snap

--- src/index.js
+++ src/index.js
@@ -1,37 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import './styles/index.scss';
 import App from './App';
 
-const rootElement = document.getElementById("root");
-const root = ReactDOM.createRoot(rootElement);
-
-if (rootElement.hasChildNodes()) {
-  root.hydrate(
-    <React.StrictMode> 
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  );
-} else {
-  root.render(
-    <React.StrictMode> 
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  );
-}
-
-/*
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode> 
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
-);*/
+);
 
